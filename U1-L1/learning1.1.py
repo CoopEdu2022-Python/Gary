f42075ef0fd27be6f62d4b470c1b8bfcddf3a511
GaryZhang123 <ztrgary@outlook.com>
2022-09-07 09:59:39 +0800

    hello world!

---
+++ U1-L1/learning1.1.py
@@ -0,0 +1,35 @@
+qq_number = '1234567' #输入qq_number可以输出值
+qq_password = '123'   #输入qq_password返回后面的值
+
+#输出qq_number and qq_passward
+print(qq_number)
+print(qq_password)
+
+#定义钱和公斤数
+price = 8.5
+weight = 7.5
+
+#最后的钱
+money = price * weight
+
+#输出
+print(money)
+
+
+price = 8.5
+weight = 7.5
+money = price* weight
+money = money -5
+print(money)
+
+i =10
+f = 10.5
+b = True
+
+print()#输出
+type()#输入
+
+a = input('输出信息') #a=想要收集的值
+
+int()#
+float()
