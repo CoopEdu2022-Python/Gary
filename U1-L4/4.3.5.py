ee23c67de76ed19bbb5025487f6b78608371b4a4
GaryZhang123 <ztrgary@outlook.com>
2022-09-27 18:53:38 +0800

    hello world!

--- U1-L4/4.3.5.py
+++ U1-L4/4.3.5.py
@@ -8,11 +8,12 @@ def h(num):
         num = num // 2 + 1
         for i in range(1, 2 * num, 2):
             a = '*' * i
-            d+=(' '*(2*num-1-i))
+            d += (' ' * (2 * num - 1 - i))
 
             r
         for n in range(2 * num - 3, 0, -2):
             c = '*' * n
+
 
 a = int(input('?'))
 print(a)
