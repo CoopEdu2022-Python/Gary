49b21b9e4de7e1b2fd5fb289d5d309c0d177af4d
GaryZhang123 <ztrgary@outlook.com>
2022-09-27 23:21:24 +0800

    hello world!

--- U1-L5/5.1.4.py
+++ U1-L5/5.1.4.py
@@ -6,8 +6,24 @@ for i in travel_list:
 travel_list.reverse()
 for i in travel_list:
     print(i)
-travel_list.sort()
+
 print(travel_list)
+for i in travel_list:
+    if i == travel_list1[travel_list1.index(i)]:
+        print('变了')
+    else:
+        print('没变')
+travel_list.reverse()
+print(travel_list)
+travel_list.sort(reverse=True)
+
+print(travel_list)
+for i in travel_list:
+    if i == travel_list1[travel_list1.index(i)]:
+        print('变了')
+    else:
+        print('没变')
+travel_list.sort()
 for i in travel_list:
     if i == travel_list1[travel_list1.index(i)]:
         print('变了')
