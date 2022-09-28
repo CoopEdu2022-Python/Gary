1e225dc551d20d3ffd44c8d5711c56c0ec365054
GaryZhang123 <ztrgary@outlook.com>
2022-09-29 07:42:22 +0800

    hello world!

--- U1-L5/5.2.8.py
+++ U1-L5/5.2.8.py
@@ -2,6 +2,6 @@ tuple_sample = (1, 1, 2, 3, 6, 5, 4, 1)
 o = list(tuple_sample)
 for i in o:
     for y in o:
-        if i == y:
+        if i == y and o.count(i) > 1:
             o.remove(i)
 print(tuple(o))
---
+++ U1-L5/Learning6.py
@@ -0,0 +1,5 @@
+
+info_tuplr = (50,)  # 元组必须有逗号
+info_tuplr.count()
+info_tuplr.index()
+#元组不能修改
