067eb09bb07904f1d54f2d684a09b4873f14191b
GaryZhang123 <ztrgary@outlook.com>
2022-10-04 23:32:56 +0800

    hello world!

--- PA1/PA1.py
+++ PA1/PA1.py
@@ -8,48 +8,47 @@ def blank_tictactoe(blank11=' ', blank12=' ', blank13=' ', blank21=' ', blank22=
     print('%3s|%3s|%3s' % (blank21, blank22, blank23))
     print('-----------')
     print('%3s|%3s|%3s' % (blank31, blank32, blank33))
-
+def panduan(a,b):
+    if a == '11':
+        blank_tictactoe(blank11=b)
+    elif a == '12':
+        blank_tictactoe(blank12=b)
+    elif a == '13':
+        blank_tictactoe(blank13=b)
+    elif a == '21':
+        blank_tictactoe(blank21=b)
+    elif a == '22':
+        blank_tictactoe(blank22=b)
+    elif a == '23':
+        blank_tictactoe(blank23=b)
+    elif a == '31':
+        blank_tictactoe(blank31=b)
+    elif a == '32':
+        blank_tictactoe(blank32=b)
+    elif a == '33':
+        blank_tictactoe(blank33=b)
+    else:
+        print('输错了')
 
 count_player1 = []
 count_player2 = []
 
-blank_tictactoe(blank11='# ')
 
-player1 = input('请输入行数和列数的组合，如第一行第一列请输入21')
-if player1 == '11':
-    blank_tictactoe(blank11='* ')
-elif player1 == '12':
-    blank_tictactoe(blank12='* ')
-elif player1 == '13':
-    blank_tictactoe(blank13='* ')
-elif player1 == '21':
-    blank_tictactoe(blank21='* ')
-elif player1 == '22':
-    blank_tictactoe(blank22='* ')
-elif player1 == '23':
-    blank_tictactoe(blank23='* ')
-elif player1 == '31':
-    blank_tictactoe(blank31='* ')
-elif player1 == '32':
-    blank_tictactoe(blank32='* ')
-elif player1 == '33':
-    blank_tictactoe(blank33='* ')
-player2 = input('请输入行数和列数的组合，如第一行第一列请输入21')
-if player1 == '11':
-    blank_tictactoe(blank11='* ')
-elif player1 == '12':
-    blank_tictactoe(blank12='* ')
-elif player1 == '13':
-    blank_tictactoe(blank13='* ')
-elif player1 == '21':
-    blank_tictactoe(blank21='* ')
-elif player1 == '22':
-    blank_tictactoe(blank22='* ')
-elif player1 == '23':
-    blank_tictactoe(blank23='* ')
-elif player1 == '31':
-    blank_tictactoe(blank31='* ')
-elif player1 == '32':
-    blank_tictactoe(blank32='* ')
-elif player1 == '33':
-    blank_tictactoe(blank33='* ')
+
+player1 = input('请输入行数和列数的组合，如第一行第一列请输入11')
+if not player1 in count_player1 and not player1 in count_player2:
+    count_player1.append(player1)
+
+else:
+    print('已经有了')
+for i in count_player1:
+    panduan(player1,'* ')
+
+player2 = input('请输入行数和列数的组合，如第一行第一列请输入11')
+if not player2 in count_player1 and not player1 in count_player2:
+    count_player2.append(player2)
+
+else:
+    print('已经有了')
+for l in count_player1:
+    panduan(l,'@ ')
---
+++ PA1/test.py
@@ -0,0 +1,4 @@
+def f(a,b):
+    print(a,b)
+f(a=1,b=2)
+f(b=1,a=2)
