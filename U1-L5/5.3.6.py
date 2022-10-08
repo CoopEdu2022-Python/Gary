4d137232a993df6943db9e3d4aeb389971a57835
GaryZhang123 <ztrgary@outlook.com>
2022-10-08 16:28:22 +0800

    hello world!

--- U1-L5/5.3.6.py
+++ U1-L5/5.3.6.py
@@ -1,11 +1,18 @@
 def compare_dict(dict1, dict2):
     a = []
-    for key in dict1:
-        if key not in dict2 or dict1[key] != dict2[key]:
+    for key,value in dict1.items():
+        if key in dict2:
+            if not value == dict2[key]:
+
+
+                a.append(key)
+                a.append(value)
+        else:
             a.append(key)
+            a.append(value)
     b = len(a)
     a.append('有' + str(b) + '个不同')
-    return a
+    return tuple(a)
 
 
 
