a8f2e4627c1b8c2df35456a10447fe7ee5c36fdc
GaryZhang123 <ztrgary@outlook.com>
2022-10-08 23:36:09 +0800

    hello world!

--- PA1/PA1.py
+++ PA1/PA1.py
@@ -1,54 +1,82 @@
 print('游戏开始啦')
 
+blank_tictactoe = {'blank0': ' ', 'blank1': ' ', 'blank2': ' ', 'blank3': ' ', 'blank4': ' ', 'blank5': ' ',
+                   'blank6': ' ', 'blank7': ' ', 'blank8': ' '}  # 变量作用打印整个表，并填上相应的值
 
-def blank_tictactoe(blank11=' ', blank12=' ', blank13=' ', blank21=' ', blank22=' ', blank23=' ', blank31=' ',
-                    blank32=' ', blank33=' '):  # 变量作用打印整个表，并填上相应的值
-    print('%3s|%3s|%3s' % (blank11, blank12, blank13))
+
+def print_tictactoe():
+    print('%3s|%3s|%3s' % (blank_tictactoe['blank0'], blank_tictactoe['blank1'], blank_tictactoe['blank2']))
     print('-----------')
-    print('%3s|%3s|%3s' % (blank21, blank22, blank23))
+    print('%3s|%3s|%3s' % (blank_tictactoe['blank3'], blank_tictactoe['blank4'], blank_tictactoe['blank5']))
     print('-----------')
-    print('%3s|%3s|%3s' % (blank31, blank32, blank33))
-def panduan(a,b):
-    if a == '11':
-        blank_tictactoe(blank11=b)
-    elif a == '12':
-        blank_tictactoe(blank12=b)
-    elif a == '13':
-        blank_tictactoe(blank13=b)
-    elif a == '21':
-        blank_tictactoe(blank21=b)
-    elif a == '22':
-        blank_tictactoe(blank22=b)
-    elif a == '23':
-        blank_tictactoe(blank23=b)
-    elif a == '31':
-        blank_tictactoe(blank31=b)
-    elif a == '32':
-        blank_tictactoe(blank32=b)
-    elif a == '33':
-        blank_tictactoe(blank33=b)
+    print('%3s|%3s|%3s' % (blank_tictactoe['blank6'], blank_tictactoe['blank7'], blank_tictactoe['blank8']))
+
+
+def decide(a, b):
+    if a == '0':
+        blank_tictactoe['blank0'] = b
+    elif a == '1':
+        blank_tictactoe['blank1'] = b
+    elif a == '2':
+        blank_tictactoe['blank2'] = b
+    elif a == '3':
+        blank_tictactoe['blank3'] = b
+    elif a == '4':
+        blank_tictactoe['blank4'] = b
+    elif a == '5':
+        blank_tictactoe['blank5'] = b
+    elif a == '6':
+        blank_tictactoe['blank6'] = b
+    elif a == '7':
+        blank_tictactoe['blank7'] = b
+    elif a == '8':
+        blank_tictactoe['blank8'] = b
     else:
         print('输错了')
+def judge():
+    if blank_tictactoe['blank0'] == blank_tictactoe['blank1'] == blank_tictactoe['blank2'] == 'X' or \
+            blank_tictactoe['blank3'] == blank_tictactoe['blank4'] == blank_tictactoe['blank5'] == 'X' or \
+            blank_tictactoe['blank6'] == blank_tictactoe['blank7'] == blank_tictactoe['blank8'] == 'X' or \
+            blank_tictactoe['blank0'] == blank_tictactoe['blank3'] == blank_tictactoe['blank6'] == 'X' or \
+            blank_tictactoe['blank1'] == blank_tictactoe['blank4'] == blank_tictactoe['blank7'] == 'X' or \
+            blank_tictactoe['blank2'] == blank_tictactoe['blank5'] == blank_tictactoe['blank8'] == 'X' or \
+            blank_tictactoe['blank0'] == blank_tictactoe['blank4'] == blank_tictactoe['blank8'] == 'X' or \
+            blank_tictactoe['blank2'] == blank_tictactoe['blank4'] == blank_tictactoe['blank6'] == 'X':
+        print('X赢了')
+        return True
+    elif blank_tictactoe['blank0'] == blank_tictactoe['blank1'] == blank_tictactoe['blank2'] == 'O' or \
+            blank_tictactoe['blank3'] == blank_tictactoe['blank4'] == blank_tictactoe['blank5'] == 'O' or \
+            blank_tictactoe['blank6'] == blank_tictactoe['blank7'] == blank_tictactoe['blank8'] == 'O' or \
+            blank_tictactoe['blank0'] == blank_tictactoe['blank3'] == blank_tictactoe['blank6'] == 'O' or \
+            blank_tictactoe['blank1'] == blank_tictactoe['blank4'] == blank_tictactoe['blank7'] == 'O' or \
+            blank_tictactoe['blank2'] == blank_tictactoe['blank5'] == blank_tictactoe['blank8'] == 'O' or \
+            blank_tictactoe['blank0'] == blank_tictactoe['blank4'] == blank_tictactoe['blank8'] == 'O' or \
+            blank_tictactoe['blank2'] == blank_tictactoe['blank4'] == blank_tictactoe['blank6'] == 'O':
+        print('O赢了')
+        return True
+
 
 count_player1 = []
 count_player2 = []
+while True:
+    player1 = input('玩家X请输入行数和列数的组合，如第一行第一列请输入0-8')
+    if not player1 in count_player1 and not player1 in count_player2:
+        count_player1.append(player1)
+        decide(player1, 'X ')
+        judge()
+        print_tictactoe()
+        break
 
+    else:
+        print('已经有了')
 
-
-player1 = input('请输入行数和列数的组合，如第一行第一列请输入11')
-if not player1 in count_player1 and not player1 in count_player2:
-    count_player1.append(player1)
-
-else:
-    print('已经有了')
-for i in count_player1:
-    panduan(player1,'* ')
-
-player2 = input('请输入行数和列数的组合，如第一行第一列请输入11')
-if not player2 in count_player1 and not player1 in count_player2:
-    count_player2.append(player2)
-
-else:
-    print('已经有了')
-for l in count_player1:
-    panduan(l,'@ ')
+while True:
+    player2 = input('玩家O请输入行数和列数的组合，如第一行第一列请输入0-8')
+    if not player2 in count_player1 and not player1 in count_player2:
+        count_player2.append(player2)
+        decide(player2, 'O ')
+        judge()
+        print_tictactoe()
+        break
+    else:
+        print('已经有了')
