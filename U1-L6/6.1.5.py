c3593eae9b5eec3bece7c992cee22f2c54432ae3
GaryZhang123 <ztrgary@outlook.com>
2022-10-18 07:46:27 +0800

    hello world!

--- U1-L6/6.1.5.py
+++ U1-L6/6.1.5.py
@@ -5,7 +5,7 @@ def U(text):
     words = 0
     num = 0
     space = 0
-    symbol = 0
+
     other = 0
     for i in text:
         if i.isalpha():
@@ -14,9 +14,8 @@ def U(text):
             num += 1
         elif i.isspace():
             space += 1
-        elif i.isidentifier():
-            symbol += 1
+
         else:
             other += 1
-    p = ('words:', words, 'num:', num, 'space:', space, 'symbol:', symbol, 'other:', other)
+    p = ('words:', words, 'num:', num, 'space:', space, 'other:', other)
     print(tuple(p))
--- U1-L6/6.1.7.py
+++ U1-L6/6.1.7.py
@@ -1,7 +1,7 @@
 o=[]
 b=''
 def j(str1,list1):
-    for i in range(len(list1)):
+    for i in range(len(list1)-1):
         o.append(list1[i]+str1)
     return b.join(o)
 
