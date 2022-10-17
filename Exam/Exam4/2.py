c43c2a990cc6e9379b4dde13cf7ee323fca5ce55
GaryZhang123 <ztrgary@outlook.com>
2022-10-17 23:40:38 +0800

    hello world!

--- Exam/Exam4/2.py
+++ Exam/Exam4/2.py
@@ -1,36 +1,14 @@
-username = input('username?')
-user_key = input('key?')
-
-
-def reset_key(username, user_key, login_info={'user1': ['pw1']}):
-    while True:
-
-        if len(user_key) <= 8:
-            print('密码不少于八位')
-        words = 'qwertyuiiiiiopasdfghjklzxcvbnm'
-        number = '1234567890'
-        y = 0
-        n = 0
-        for i in user_key:
-            if i.lower in words:
-                y += 1
-            if i in number:
-                n += 1
-        if user_key == 'Q':
-            break
-        if y < 1:
-            print('没有字母')
-            continue
-        if n < 1:
-            print('没有数字')
-            continue
-        if user_key in login_info[username]:
-            print('请设置未使用的密码')
+def reset(login_info):
+    while user_name:=input('?'):
+        if user_name == 'q':
+            return False
+        if user_name not in login_info.keys():
+            print('?')
         else:
-            print('已重置密码')
-            login_info[username].append(user_key)
-            print(tuple(username, user_key))
-        if len(login_info[username]) == 4:
-            login_info[username].pop(0)
-        return False
+            break
+    while password:=input('?'):
+        if user_name == 'q':
+            return False
+        if len(password)<8:
+            print('')
 
---
+++ U1-L7/Learning7.py
@@ -0,0 +1,17 @@
+item='sdfffffffffffffff'
+len(item)
+del(item)
+max(item)
+min(item)
+item='sdfffffffffffffff'
+item[::-1]
+item1='sdfffffffffffffff'
+item2='sd'
+item1+item2
+item1*2
+f in item1
+f not in item1
+for i in item1:
+    print(i)
+else:
+    print('end')#如果用了break，就不会执行else
