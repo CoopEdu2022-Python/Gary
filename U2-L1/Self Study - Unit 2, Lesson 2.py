688f36e45753637079ede2c7972557e5bbb9803e
GaryZhang123 <ztrgary@outlook.com>
2022-10-26 00:42:16 +0800

    hello world!

---
+++ U2-L1/Self Study - Unit 2, Lesson 2.py
@@ -0,0 +1,22 @@
+# 1. [多行注释] Python 的 self 是什么？
+'''
+self代表对象本身
+'''
+
+# 2. [多行注释] Python 类的内置方法有哪些？
+'''
+__init__ 
+__del__ 
+__repr__ 
+__setitem__ 
+__getitem__
+__len__
+__cmp__
+__call__
+__add__
+__sub__
+__mul__
+__truediv__
+__mod__
+__pow__
+'''
