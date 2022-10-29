2209a81cac18256114f8ad71186ca252286e4c66
GaryZhang123 <ztrgary@outlook.com>
2022-10-29 22:59:41 +0800

    hello world!

--- PA1/PA1.py
+++ PA1/PA1.py
@@ -39,7 +39,7 @@ def decide(a, b):
         blank_tictactoe['blank8'] = b
         return True
     else:
-        print('输错了')
+        print('输错了4')
         return False
 
 
@@ -53,6 +53,7 @@ def judge():
             blank_tictactoe['blank0'] == blank_tictactoe['blank4'] == blank_tictactoe['blank8'] == 'X ' or \
             blank_tictactoe['blank2'] == blank_tictactoe['blank4'] == blank_tictactoe['blank6'] == 'X ':
         print('\033[5;36;47m玩家X赢了\033[0m')
+        time.sleep(5)
         return True
     elif blank_tictactoe['blank0'] == blank_tictactoe['blank1'] == blank_tictactoe['blank2'] == 'O ' or \
             blank_tictactoe['blank3'] == blank_tictactoe['blank4'] == blank_tictactoe['blank5'] == 'O ' or \
@@ -63,6 +64,7 @@ def judge():
             blank_tictactoe['blank0'] == blank_tictactoe['blank4'] == blank_tictactoe['blank8'] == 'O ' or \
             blank_tictactoe['blank2'] == blank_tictactoe['blank4'] == blank_tictactoe['blank6'] == 'O ':
         print('\033[5;36;47m玩家O赢了\033[0m')
+        time.sleep(5)
         return True
 
 
@@ -75,33 +77,52 @@ while True:
     count_playerXO = []
     os.system('cls')
     print_tictactoe()
+    chose_mode = input('请选择模式(1)普通玩法 (2)进阶玩法')
+    if chose_mode not in ['1', '2']:
+        print('输错了90')
+        time.sleep(5)
+        continue
+    win=bool()
     while True:
+
 
         while True:
             player1 = input('玩家X请输入行数和列数的组合，如第一行第一列请输入0-8')
-            if player1 == '0' or player1 == '1' or player1 == '2' or player1 == '3' or player1 == '4' or player1 == '5' \
-                    or player1 == '6' or player1 == '7' or player1 == '8':
-
+            if player1 in ['0', '1', '2', '3', '4', '5', '6', '7', '8']:
+                os.system('cls')
                 if player1 not in count_playerXO:
-                    if len(count_playerXO) >= 7:
-                        a = str(count_playerXO.pop(0))
-                        print('消除了', a, 'X')
-                        blank_tictactoe['blank' + a] = ' '
+                    if chose_mode == '1':
+                        if len(count_playerXO) > 8:
+                            if not judge():
+                                print('平局')
+                                time.sleep(5)
+                                break
+                    elif chose_mode == '2':
+
+                        if len(count_playerXO) > 5:
+                            a = str(count_playerXO.pop(0))
+                            if not judge():
+                                print('消除了', a, 'X')
+                                time.sleep(5)
+                            blank_tictactoe['blank' + a] = ' '
+                    else:
+                        print('输入模式错误')
 
                     if decide(player1, 'X '):
                         count_playerXO.append(player1)
 
                     win = judge()
-                    os.system('cls')
+
                     print_tictactoe()
 
                     break
 
                 else:
                     os.system('cls')
-                    print('\033[1;31;47m已经有了\033[0m')
+                    print('\033[1;31;47m已经有了3\033[0m')
 
                     print_tictactoe()
+
                 if win:
                     os.system('cls')
                     print_tictactoe()
@@ -111,37 +132,48 @@ while True:
 
             else:
                 os.system('cls')
-                print('\033[1;31;47m输错了\033[0m')
+                print('\033[1;31;47m输错了2\033[0m')
                 print_tictactoe()
 
         if win:
             print('\033[5;36;47m玩家X赢了\033[0m')
+            time.sleep(5)
             break
 
         while True:
             player2 = input('玩家O请输入行数和列数的组合，如第一行第一列请输入0-8')
-            if player2 == '0' or player2 == '1' or player2 == '2' or player2 == '3' or player1 == '4' or player1 == '5' \
-                    or player2 == '6' or player2 == '7' or player1 == '8':
+            if player2 in ['0', '1', '2', '3', '4', '5', '6', '7', '8']:
 
                 if player2 not in count_playerXO:
+                    os.system('cls')
+                    if chose_mode == '1':
+                        if len(count_playerXO) > 8:
+                            if not judge():
+                                print('平局')
+                                break
+                    elif chose_mode == '2':
 
-                    if len(count_playerXO) >= 7:
-                        a = str(count_playerXO.pop(0))
-                        print('消除了', a, 'O')
-                        print(a)
+                        if len(count_playerXO) > 5:
+                            a = str(count_playerXO.pop(0))
+                            if not judge():
+                                print('消除了', a, 'O')
+                                time.sleep(5)
+                            blank_tictactoe['blank' + a] = ' '
+                    else:
+                        print('输入模式错误')
 
                     if decide(player2, 'O '):
                         count_playerXO.append(player2)
 
                     win = judge()
-                    os.system('cls')
+
                     print_tictactoe()
 
                     break
 
                 else:
                     os.system('cls')
-                    print('已经有了')
+                    print('已经有了9')
 
                     print_tictactoe()
 
@@ -154,8 +186,11 @@ while True:
 
             else:
                 os.system('cls')
-                print('\033[1;31;47m输错了\033[0m')
+                print('\033[1;31;47m输错了1\033[0m')
+
                 print_tictactoe()
+                time.sleep(5)
         if win:
             print('\033[5;36;47m玩家O赢了\033[0m')
+            time.sleep(5)
             break
