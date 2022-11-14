02946b46ca799979895b2ae65d8a5f4f0099182f
GaryZhang123 <ztrgary@outlook.com>
2022-11-15 07:50:31 +0800

    hello world!

--- Exam/Exam7/1.py
+++ Exam/Exam7/1.py
@@ -4,22 +4,24 @@ def gcd_pro_max_2(*n):
     while h[0] != h[-1]:
 
         for i in range(len(n)-1):
-            if h[i]//h[i+1]==1:
+            if h[i]%h[i+1]==0:
                 h[i]=h[i+1]
             else:
-                h[i] = h[i] % h[i+1]
+                h[i] = h[i] - h[i+1]
     return h[0]
 def gcd_pro_max_1(*n):
     h = sorted(n)
     h = h[::-1]
-    if h[0] != h[-1]:
-        for i in range(len(n)-1):
-            if h[i]//h[i+1]==1:
-                h[i]=h[i+1]
-            else:
-                h[i] = h[i] % h[i+1]
-                return gcd_pro_max_1(*h)
-    else:
+    if h[0]!=h[-1]:
+        return h[0]
+
+    for i in range(len(n)-1):
+        if h[i]%h[i+1]==0:
+            h[i]=h[i+1]
+        else:
+            h[i] = h[i] - h[i+1]
+            return gcd_pro_max_1(*h)
+
         return h[0]
 
 
