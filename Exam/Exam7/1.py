23b23a7021d6a62ae2b7f2d10a2918e0907720c8
GaryZhang123 <ztrgary@outlook.com>
2022-11-14 17:02:40 +0800

    hello world!

--- Exam/Exam7/1.py
+++ Exam/Exam7/1.py
@@ -1,12 +1,17 @@
 def gcd_pro_max_2(*n):
-    h = sorted(n)
-    a=0
-    for i,p in enumerate(h):
-        if h[i]//h[i+1]==1:
-            a+=1
+    while
+        a=0
+        h = sorted(n)
+        for i in n:
+            a+=i
+        if h[0]*len(n)==a:
+            return h[0]
+        for i,p in enumerate(h):
+            if h[i]//h[i+1]==1:
+                pass
 
-        else:
-            h[i] = h[i] % h[i+1]
+            else:
+                h[i] = h[i] % h[i+1]
 
 
 
