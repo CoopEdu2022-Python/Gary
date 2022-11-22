e9267d6713fcdc5c748d247727c784c218764c4f
GaryZhang123 <ztrgary@outlook.com>
2022-11-22 23:10:49 +0800

    hello world!

--- Exam/Exam8/5.py
+++ Exam/Exam8/5.py
@@ -1,8 +1,8 @@
 def largest_perimeter(nums: list) -> int:
     nums.sort()
     a=nums[0]+nums[1]
-    if a<nums[2]:
+    if a<=nums[2]:
         return 0
     else:
         return nums[0]+nums[1]+nums[2]
-print(largest_perimeter([1,2,1]))
+print(largest_perimeter([2,1,2]))
