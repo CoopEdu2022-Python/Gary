168cad1d030caa9e06e4876772cb9f03b7cfa91d
GaryZhang123 <ztrgary@outlook.com>
2022-12-04 16:45:24 +0800

    hello world!

--- U2-L12/bad_boy.py
+++ U2-L12/bad_boy.py
@@ -1,13 +1,13 @@
 a = 1
 for i in range(10):
-   a += 1
-   print(a)
+    a += 1
+    print(a)
 for i in range(10):
     a -= 1
     print(a)
     if a == 5:
-      print(a)
-      break
+        print(a)
+        break
     else:
         if a == 7:
             print(a)
