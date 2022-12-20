5b264ac8bb1fe93616fbefdec85ab7bbddad1efd
GaryZhang123 <ztrgary@outlook.com>
2022-12-20 08:54:09 +0800

    hello world!

--- PA3/day2.1.py
+++ PA3/day2.1.py
@@ -2,8 +2,13 @@ import sys
 import pygame
 
 pygame.init()
+
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Hello World")
+pygame.mixer.init()
+pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play(-1, 0.0)
+
 pygame.draw.rect(screen, (0, 0, 0), (0, 0, 640, 480))
 pygame.draw.polygon(screen, (0, 0, 255), ((0, 0), (640, 0), (640, 480), (0, 480)))
 pygame.draw.circle(screen, (255, 0, 0), (320, 240), 100)
@@ -14,7 +19,6 @@ pygame.draw.lines(screen, (0, 255, 255), True, ((100, 100), (200, 100), (200, 20
 pygame.draw.aaline(screen, (255, 255, 255), (100, 100), (200, 100))
 pygame.draw.aalines(screen, (255, 255, 255), True, ((100, 100), (200, 100), (200, 200), (100, 200)))
 while True:
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
